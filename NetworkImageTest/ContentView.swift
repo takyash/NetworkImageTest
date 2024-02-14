3c3edfced7022654fcf976c94daf9bfdcb74ffb7
Yash Tak <yash.tak1998@gmail.com>
2024-02-15 04:49:56 +0530

    Initial Commit

---
+++ NetworkImageTest/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  NetworkImageTest
+//
+//  Created by Yash Tak on 14/02/24.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct ContentView: View {
+
+    private let defaultImg = "music.note"
+
+    private let imgURL = "https://i.scdn.co/image/ab67616d0000b2739933d951d1318d4a11d7766e"
+
+    @State
+    private var hasFailed = false
+
+    var body: some View {
+
+        ZStack {
+            
+            Color(.black)
+                .ignoresSafeArea(.all)
+
+            KFImage.url(URL(string: "\(imgURL)h"), cacheKey: imgURL)
+                .placeholder({ progress in
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                })
+                .onSuccess({ result in
+                    print("Image Load Complete")
+                    hasFailed = false
+                })
+                .onFailure({ error in
+                    print("Failed To Load From URL")
+                    print("Before -> \(hasFailed)")
+                    hasFailed = true
+                    print("After -> \(hasFailed)")
+                })
+                .fade(duration: 0.5)
+                .retry(maxCount: 1)
+                .cancelOnDisappear(true)
+                .cacheOriginalImage(true)
+                .waitForCache(true)
+                .fromMemoryCacheOrRefresh(true)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+
+            if(hasFailed) {
+
+                Image(systemName: defaultImg)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+        .frame(width: 100, height: 100)
+        .overlay {
+            RoundedRectangle(cornerRadius: 100/8)
+                .stroke(.white, lineWidth: 2.0)
+        }
+    }
+}
+
+#Preview {
+
+    ContentView()
+        .preferredColorScheme(.dark)
+}
---
+++ NetworkImageTest/NetworkImageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkImageTestApp.swift
+//  NetworkImageTest
+//
+//  Created by Yash Tak on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkImageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
